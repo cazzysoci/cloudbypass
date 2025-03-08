ae50ab16140dc352a95b89d1a44a4ac3f7fc33c4
cazzysoci <carielglobio@gmail.com>
2025-03-08 10:15:59 +0800

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+os.system('pip3 install dnspython')
+os.system('pip3 install whois')
+os.system('pip3 install termcolor')
+os.system('pip3 install python-nmap')
+
+print(f"\x1b[31m[\x1b[33mcazbotnet\x1b[31m]\x1b[0m \x1b[32m> \x1b[33mInstallation Successful")
